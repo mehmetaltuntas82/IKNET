950f314de919ee1ffeea1889b2e77955c29d6b4f
The-Gencturk <eymengencturk2008@gmail.com>
2026-02-12 15:24:05 +0300

    hbhnjh

--- MenuLoader.js
+++ MenuLoader.js
@@ -1,23 +1,28 @@
-document.addEventListener("DOMContentLoaded", async () => {
-
-    const container = document.getElementById("UpMenu");
-
-    if (!container) return;
-
-    try {
-
-        const res = await fetch("UpMenu.html");
-        const html = await res.text();
-
-        container.innerHTML = html;
-
-        // Menü geldikten sonra başlat
-        initAdminMenu();
-
-    } catch (err) {
-
-        console.error("Menü yüklenemedi:", err);
-
-    }
-
+document.addEventListener("DOMContentLoaded", () => {
+  loadMenu("Footer", "Footer.html");
+  loadMenu("Header", "Header.html");
 });
+
+
+async function loadMenu(containerId, file, callback) {
+
+  const container = document.getElementById(containerId);
+
+  if (!container) return;
+
+  try {
+
+    const res = await fetch(file);
+    const html = await res.text();
+
+    container.innerHTML = html;
+
+    if (callback) callback();
+
+  } catch (err) {
+
+    console.error(file + " yüklenemedi:", err);
+
+  }
+
+}
